fef0f1e78a3a80c9a439ecf8363eb6335352aff5
S-Flavius <76589861+S-Flavius@users.noreply.github.com>
2021-05-12 19:16:51 +0200

    Update redirector.user.js

--- redirector.user.js
+++ redirector.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SH redirect
 // @namespace    https://github.com/S-Flavius/SH-Redirector
-// @version      1.12
+// @version      1.1
 // @description  Redirect button to SH guides from TSA
 // @author       Rederdex
 // @match        https://truesteamachievements.com/game/*/achievements*
@@ -21,8 +21,13 @@
     var place = document.querySelector("#frm > div.page.tsa > div.main.middle > aside > section.navy.gameinfo > article");
     place.appendChild(button);
 
-    var appid = document.querySelector("#frm > div.page.tsa > div.main.middle > aside > section.navy.gameinfo > article > dl > dd:nth-child(18) > a").text;
-    appid = appid.substring(0, appid.length-1);
+    var appidString = document.querySelector("#frm > div.page.tsa > div.main.middle > aside > section.navy.gameinfo > article > dl > dd:last-child > a").text;
+    var appid = "";
+    for ( var i = 0; i < appidString.length; i++){
+        if ( !appidString[i].match(/[a-z]/i)){
+            appid += appidString[i];
+        }    
+    }
 
     var shLink = "https://steamhunters.com/stats/" + appid + "/guides/add";
     button.addEventListener("click", function(){
