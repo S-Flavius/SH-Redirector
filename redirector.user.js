d24b4499c1b5566b9602292be25bbf9a3314ed2f
S-Flavius <76589861+S-Flavius@users.noreply.github.com>
2021-05-12 18:55:25 +0200

    Update redirector.user.js

--- redirector.user.js
+++ redirector.user.js
@@ -1,3 +1,15 @@
+// ==UserScript==
+// @name         SH redirect
+// @namespace    https://github.com/S-Flavius/SH-Redirector
+// @version      1.1
+// @description  Redirect button to SH guides from TSA
+// @author       Rederdex
+// @match        https://truesteamachievements.com/game/*/achievements*
+// @updateURL    https://github.com/S-Flavius/SH-Redirector/raw/main/redirector.meta.js
+// @downloadURL  https://github.com/S-Flavius/SH-Redirector/raw/main/redirector.user.js
+// @grant        none
+// ==/UserScript==
+
 (function() {
     var button = document.createElement("button");
     button.innerHTML = "Steam Hunters";
