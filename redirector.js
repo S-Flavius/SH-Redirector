1e792c158fb59a0d02c301287d6c7874dc82200d
S-Flavius <76589861+S-Flavius@users.noreply.github.com>
2021-05-12 14:34:54 +0200

    Update redirector.js

--- redirector.js
+++ redirector.js
@@ -4,12 +4,12 @@
 // @version      1.1
 // @description  Redirect button to SH guides from TSA
 // @author       Rederdex
-// @match        https://truesteamachievements.com/game/*/achievements
-// @updateURL    https://raw.githubusercontent.com/S-Flavius/SH-Redirector/main/redirector.js
-// @downloadURL  https://raw.githubusercontent.com/S-Flavius/SH-Redirector/main/redirector.js
+// @match        https://truesteamachievements.com/game/*/achievements*
+// @updateURL    https://gist.github.com/S-Flavius/75db886199441fe5bd1f17dc1579c7c0/raw/905f68bdac7d3254b5adfab05370c8c747c93e2c/sh-guide-redirector.user.js
+// @downloadURL  https://gist.github.com/S-Flavius/75db886199441fe5bd1f17dc1579c7c0/raw/905f68bdac7d3254b5adfab05370c8c747c93e2c/sh-guide-redirector.user.js
 // @grant        none
-// @run-at       document-end
 // ==/UserScript==
+
 
 (function() {
     var button = document.createElement("button");
